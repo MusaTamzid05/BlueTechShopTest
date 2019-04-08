74e6dc411f036363b3a9f0458e6528fc332b56af
Musa <tamzid101@gmail.com>
2019-04-08 16:35:38 +0600

    function for lising twitter handler for reviews is added.

--- python_test/review_table.py
+++ python_test/review_table.py
@@ -41,5 +41,53 @@ class ReviewTable:
             connection.close()
 
 
+    @staticmethod
+    def get_users_with_review_more_equal_to(count):
+        connection = Database.get_connection()
+
+        if connection is None:
+            return 0
+
+        cur = connection.cursor()
+        try:
+            cur.execute("SELECT taster_twitter_handle, COUNT(*) from reviews GROUP BY taster_twitter_handle HAVING COUNT(*) >= %s;" ,(count , ) )
+            return cur.fetchall()
+        except AttributeError as e:
+            print(e)
+            return 0
+        finally:
+
+            cur.close()
+            connection.close()
+
+
+    @staticmethod
+    def get_unique_reviewers_twitter():
+        connection = Database.get_connection()
+
+        if connection is None:
+            return []
+        cur = connection.cursor()
+        try:
+            cur.execute("SELECT DISTINCT taster_twitter_handle  FROM reviews;" )
+            user_data = cur.fetchall()
+
+            if len(user_data) == 0:
+                return []
+
+            twitter_handle = []
+
+            for data in user_data:
+                if data[0] is not None:
+                    twitter_handle.append(data[0])
+
+            return twitter_handle
+
+        except AttributeError as e:
+            print(e)
+            return []
+        finally:
+            cur.close()
+            connection.close()
 
 
